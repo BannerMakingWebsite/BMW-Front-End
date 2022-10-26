13405772c018c59a46d3949f852e5d54cdfdba02
justinlee05 <justinlee05@naver.com>
2022-10-26 19:39:59 +0900

    📝 :: Sidebar Update

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,25 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { pxToRem } from "../../assets/constants/pxToRem";
 import { Icons } from "../../assets/images";
 
 function Sidebar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <Background>
       <IconsWrapper>
-        <Arrow>
-          <img src={Icons.MenuArrow} />
+        <Arrow turn={location.pathname == "/"} onClick={() => navigate("/")}>
+          <img src={SidebarIcons.MenuArrow} />
         </Arrow>
-        <Icon src={Icons.Template} name="템플릿" />
-        <Icon src={Icons.Image} name="이미지" />
-        <Icon src={Icons.Figure} name="도형" />
+        <Icon src={SidebarIcons.Template} name="템플릿" path="/template" />
+        <Icon src={SidebarIcons.Image} name="이미지" path="/image" />
+        <Icon src={SidebarIcons.Figure} name="도형" path="/figure" />
       </IconsWrapper>
       <IconsWrapper>
-        <Icon src={Icons.Account} name="계정" />
-        <Icon src={Icons.Setting} name="설정" />
+        <Icon src={SidebarIcons.Account} name="계정" path="/account" />
+        <Icon src={SidebarIcons.Setting} name="설정" path="/setting" />
       </IconsWrapper>
     </Background>
   );
@@ -25,11 +29,18 @@ export default Sidebar;
 interface IconProps {
   name: string;
   src: string;
-  onClick?: () => void;
+  path: string;
 }
-function Icon({ name, src, onClick }: IconProps) {
+function Icon({ name, src, path }: IconProps) {
+  const navigate = useNavigate();
   return (
-    <IconWrapper>
+    <IconWrapper
+      onClick={() => navigate(path)}
+      style={{
+        backgroundColor:
+          window.location.pathname == path ? "rgba(255,255,255,0.3)" : "unset",
+      }}
+    >
       <img src={src} alt="" />
       <p>{name}</p>
     </IconWrapper>
@@ -37,20 +48,21 @@ function Icon({ name, src, onClick }: IconProps) {
 }
 
 const Background = styled.div`
-  z-index: 900;
-  position: fixed;
-  height: calc(100vh - 4rem);
-  width: 5rem;
-  left: 0px;
-  bottom: 0px;
   background-color: ${({ theme }) => theme.colors.bg1f};
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: ${pxToRem(80)}rem;
+  height: calc(100vh - ${pxToRem(64)}rem);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  z-index: 900;
 `;
 
 const IconsWrapper = styled.div`
+  padding: ${pxToRem(26)}rem 0;
   width: 100%;
   height: fit-content;
   display: flex;
@@ -60,25 +72,31 @@ const IconsWrapper = styled.div`
 `;
 
 const IconWrapper = styled.div`
-  width: ${pxToRem(45)}rem;
-  height: ${pxToRem(50)}rem;
-  font-size: 14px;
-  color: ${({ theme }) => theme.colors.white};
+  width: 100%;
+  height: ${pxToRem(85)}rem;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  :not(:last-child) {
-    margin-bottom: ${pxToRem(40)}rem;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSizes.description};
+  transition: filter 0.25s ease;
+  ${({ theme }) => theme.common.hoverEffect}
+  transition: 0.2s;
+
+  > p {
+    margin-top: ${pxToRem(5)}rem;
   }
 `;
 
-const Arrow = styled.div`
+const Arrow = styled.div<{ turn: boolean | any }>`
+  margin-bottom: ${pxToRem(28)}rem;
   width: ${pxToRem(45)}rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  cursor: pointer;
-  margin-bottom: ${pxToRem(38)}rem;
+  transition: filter 0.25s ease;
+  ${({ theme }) => theme.common.hoverEffect};
+  transform: ${(props) => props.turn && "rotate(180deg)"};
+  transition: 0.2s;
 `;
