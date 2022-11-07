f780cb8e8c32b8254994fb5888988067acab05e7
Sonnehilda <winhave@naver.com>
2022-11-07 19:12:30 +0900

    📑 :: Collection 컴포넌트 추가

---
+++ src/assets/constants/scroll.ts
@@ -0,0 +1,56 @@
+export const scrollNext = (refObj: React.MutableRefObject<HTMLDivElement>) => {
+  const sum: number =
+    refObj.current.scrollLeft -
+    (refObj.current.scrollWidth - refObj.current.clientWidth);
+  if (sum < 2 && sum > -2)
+    refObj.current.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  else if (
+    refObj.current.scrollLeft +
+      (refObj.current.scrollWidth - refObj.current.clientWidth) / 4 >
+    refObj.current.scrollWidth - refObj.current.clientWidth
+  )
+    refObj.current.scrollTo({
+      top: 0,
+      left: refObj.current.scrollWidth - refObj.current.clientWidth,
+      behavior: "smooth",
+    });
+  else
+    refObj.current.scrollTo({
+      top: 0,
+      left:
+        refObj.current.scrollLeft +
+        (refObj.current.scrollWidth - refObj.current.clientWidth) / 4,
+      behavior: "smooth",
+    });
+};
+
+export const scrollPrev = (refObj: React.MutableRefObject<HTMLDivElement>) => {
+  if (refObj.current.scrollLeft === 0)
+    refObj.current.scrollTo({
+      top: 0,
+      left: refObj.current.scrollWidth - refObj.current.clientWidth,
+      behavior: "smooth",
+    });
+  else if (
+    refObj.current.scrollLeft +
+      (refObj.current.scrollWidth - refObj.current.clientWidth) / 4 <
+    0
+  )
+    refObj.current.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  else
+    refObj.current.scrollTo({
+      top: 0,
+      left:
+        refObj.current.scrollLeft -
+        (refObj.current.scrollWidth - refObj.current.clientWidth) / 4,
+      behavior: "smooth",
+    });
+};
