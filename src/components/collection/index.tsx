b3d54d76218bd005b67a85fd9beb9632b0cb09a7
Sonnehilda <winhave@naver.com>
2022-11-15 20:12:50 +0900

    🐛 :: Collection 컴포넌트 버그 수정 및 모달 창 표시 기능 추가

--- src/components/collection/index.tsx
+++ src/components/collection/index.tsx
@@ -1,31 +1,56 @@
 import { useRef } from "react";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { pxToRem } from "../../assets/constants/pxToRem";
 import { scrollNext, scrollPrev } from "../../assets/constants/scroll";
+import { modalStateAtom } from "../../atoms/modalState";
+import ModalContentsViewTemplate from "../Modal/viewTemplate";
 
 interface CollectionProps {
   title: string;
 }
 
 const Collection = ({ title }: CollectionProps) => {
+  const [modalState, setModalState] = useRecoilState(modalStateAtom);
+
   const wrapperRef = useRef<HTMLDivElement>(null);
   return (
     <Background>
-      <TitleWrapper>
+      <Title>
         <h1>{title}</h1>
         <span>더 보기 →</span>
-      </TitleWrapper>
-      <Wrapper ref={wrapperRef}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      </Title>
+      <Wrapper>
+        <ScrollWrapper>
+          <div onClick={() => scrollPrev(wrapperRef)}>◀</div>
+          <div onClick={() => scrollNext(wrapperRef)}>▶</div>
+        </ScrollWrapper>
+        <CollectionWrapper ref={wrapperRef}>
+          <div
+            onClick={() =>
+              setModalState({
+                title: "템플릿 정보",
+                modalContents: (
+                  <ModalContentsViewTemplate
+                    id={0}
+                    title={""}
+                    tags={[]}
+                    author={""}
+                    date={""}
+                    like={0}
+                    favorite={0}
+                    comments={[]}
+                  />
+                ),
+              })
+            }
+          ></div>
+          <div />
+          <div />
+          <div />
+          <div />
+        </CollectionWrapper>
       </Wrapper>
-      <ScrollWrapper>
-        <div onClick={() => scrollPrev(wrapperRef)}>◀</div>
-        <div onClick={() => scrollNext(wrapperRef)}>▶</div>
-      </ScrollWrapper>
     </Background>
   );
 };
@@ -37,7 +62,7 @@ const Background = styled.div`
   height: 14rem;
 `;
 
-const TitleWrapper = styled.div`
+const Title = styled.div`
   width: 100%;
 
   display: flex;
@@ -60,9 +85,19 @@ const TitleWrapper = styled.div`
 `;
 
 const Wrapper = styled.div`
-  padding-left: ${pxToRem(30)}rem;
-  padding-right: ${pxToRem(20)}rem;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CollectionWrapper = styled.div`
+  position: absolute;
+
   margin-top: ${pxToRem(10)}rem;
+
+  width: calc(100% - ${pxToRem(60)}rem);
+  height: ${pxToRem(175)}rem;
 
   display: flex;
 
@@ -85,33 +120,34 @@ const Wrapper = styled.div`
     margin-right: ${pxToRem(10)}rem;
 
     min-width: ${pxToRem(210)}rem;
-    height: ${pxToRem(175)}rem;
 
     border-radius: 1.5rem;
+    transition: filter 0.25s ease;
+    cursor: pointer;
+
+    :hover {
+      filter: brightness(85%);
+    }
+
+    :last-of-type {
+      margin-right: 0;
+    }
   }
 `;
 
 const ScrollWrapper = styled.div`
-  @media screen and (max-width: 400px) {
-    display: none !important;
-  }
-
-  transform: translateY(${pxToRem(190.5) * -1}rem);
-
   margin-top: 0.5rem;
 
   width: 100%;
+  height: ${pxToRem(175)}rem;
 
   display: flex;
   justify-content: space-between;
-
-  z-index: 0;
 
   > div {
     background-color: ${({ theme }) => theme.colors.bg5f};
 
     width: ${pxToRem(20)}rem;
-    height: ${pxToRem(175)}rem;
 
     display: flex;
     justify-content: center;
