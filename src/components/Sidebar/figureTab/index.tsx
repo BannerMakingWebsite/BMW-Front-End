64de824f707a03fda01b7514229974fa1c029687
justinlee05 <justinlee05@naver.com>
2022-11-16 12:32:24 +0900

    📝 :: 도형 생성 기능 추가 및 코드 정리

--- src/components/Sidebar/figureTab/index.tsx
+++ src/components/Sidebar/figureTab/index.tsx
@@ -2,9 +2,87 @@ import styled from "styled-components";
 import { pxToRem } from "../../../assets/constants/pxToRem";
 import { FigureIcons } from "../../../assets/images";
 import Head from "../head";
-
+import {
+  FigureDataType,
+  TextDataType,
+} from "../../../assets/types/elementTypes";
+import { useRecoilState } from "recoil";
+import { ElementListState } from "../../../atoms/elementState";
 function FigureTab() {
-  const FigureText = ["사각형", "원형", "삼각형", "텍스트"];
+  const [elementList, setElementList] = useRecoilState(ElementListState);
+
+  const getNewFigure = (type: string): TextDataType | FigureDataType => {
+    if (type != "text")
+      return {
+        id: self.crypto.randomUUID(),
+        type: type,
+        height: 100,
+        width: 100,
+        posX: 0,
+        posY: 0,
+        flipX: false,
+        flipY: false,
+        color: "green",
+        borderColor: "black",
+        opacity: 100,
+        shadowColor: "",
+      };
+    return {
+      id: self.crypto.randomUUID(),
+      type: "text",
+      width: 100,
+      height: 100,
+      posX: 0,
+      posY: 0,
+      flipX: false,
+      flipY: false,
+      color: "black",
+      backgroundColor: "",
+      borderColor: "black",
+      borderWidth: 1,
+      fontAlign: "start",
+      fontFamily: "unset",
+      fontSize: 13,
+      fontStyle: [],
+      opacity: 100,
+      shadowColor: "",
+      shadowDirection: 0,
+      shadowDistance: 0,
+      shadowOpacity: 0,
+      value: "텍스트를 입력하세요.",
+    };
+  };
+
+  const FigureText = [
+    {
+      text: "사각형",
+      img: FigureIcons.Square,
+      onClick: () => {
+        setElementList([...elementList, getNewFigure("square")]);
+      },
+    },
+    {
+      text: "원형",
+      img: FigureIcons.Circle,
+      onClick: () => {
+        setElementList([...elementList, getNewFigure("circle")]);
+      },
+    },
+    {
+      text: "삼각형",
+      img: FigureIcons.Triangle,
+      onClick: () => {
+        setElementList([...elementList, getNewFigure("triangle")]);
+      },
+    },
+    {
+      text: "텍스트",
+      img: FigureIcons.Text,
+      onClick: () => {
+        setElementList([...elementList, getNewFigure("text")]);
+      },
+    },
+  ];
 
   return (
     <Background>
@@ -12,18 +90,9 @@ function FigureTab() {
       <ContentWrapper>
         {FigureText.map((figure) => {
           return (
-            <FigureWrapper>
-              {figure === "사각형" && (
-                <img src={FigureIcons.Square} alt="square" />
-              )}
-              {figure === "원형" && (
-                <img src={FigureIcons.Circle} alt="circle" />
-              )}
-              {figure === "삼각형" && (
-                <img src={FigureIcons.Triangle} alt="triangle" />
-              )}
-              {figure === "텍스트" && <img src={FigureIcons.Text} alt="text" />}
-              <h1>{figure}</h1>
+            <FigureWrapper onClick={figure.onClick}>
+              <img src={figure.img} alt="square" />
+              <h1>{figure.text}</h1>
             </FigureWrapper>
           );
         })}
