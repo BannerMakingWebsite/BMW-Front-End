0f059ec0d687663722604b0b45137969a842907a
yoon-ho7 <suho0317@gmail.com>
2022-11-22 18:26:53 +0900

    ⚡️ :: 템플릿 공유 기능 추가

--- src/components/Modal/shareTemplate/index.tsx
+++ src/components/Modal/shareTemplate/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { pxToRem } from "../../../assets/constants/pxToRem";
@@ -6,22 +6,46 @@ import { modalStateAtom } from "../../../atoms/modalState";
 import Button from "../button";
 import ModalInput from "../input";
 import ModalContentsTerms from "../terms";
+import { TempUpload } from "../../../apis/tempUpload";
+import ElementListState, { CaptureRefState } from "../../../atoms/elementState";
+import html2canvas from "html2canvas";
 
 function ModalContentsShareTemplate() {
   const [modalState, setModalState] = useRecoilState(modalStateAtom);
-
+  const [elementList] = useRecoilState(ElementListState);
+  const [ref] = useRecoilState(CaptureRefState);
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [prev, setPrev] = useState("");
+
+  useEffect(() => {
+    const element = ref.current;
+    console.log(inputRef.current);
+    html2canvas(element).then((value) => {
+      console.log(value.toDataURL("image/jpg"));
+      setPrev(value.toDataURL("image/jpg"));
+    });
+  }, [inputRef.current]);
+
+  const onPost = () => {
+    TempUpload(
+      "template",
+      JSON.stringify(elementList),
+      inputRef.current.value,
+      prev
+    );
+  };
 
   return (
     <>
       <Background>
-        <img
-          src={"https://blog.hubspot.com/hubfs/design-1.jpg"}
-          alt="template"
+        <img src={prev} alt="template" onClick={onPost} />
+        <ModalInput
+          type="big"
+          refObj={inputRef}
+          label="템플릿 제목"
+          id="template-title"
         />
-
-        <ModalInput type="big" label="템플릿 제목" id="template-title" />
-
         <ModalInput
           type="checkbox"
           label={"BMW의 *이용 약관*에 동의합니다."}
@@ -35,7 +59,11 @@ function ModalContentsShareTemplate() {
           }
         />
 
-        <div>
+        <div
+          onClick={() => {
+            onPost();
+          }}
+        >
           <Button type="big" label="게시" refObj={buttonRef} />
         </div>
       </Background>
@@ -48,13 +76,10 @@ export default ModalContentsShareTemplate;
 const Background = styled.div`
   display: flex;
   flex-direction: column;
-
   img {
     margin-bottom: ${pxToRem(25)}rem;
-
     width: ${pxToRem(624)}rem;
     height: ${pxToRem(425)}rem;
-
     border-radius: 1.5rem;
     object-fit: cover;
   }
